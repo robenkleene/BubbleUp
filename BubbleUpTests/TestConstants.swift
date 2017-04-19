f6002a82da037379f15e0404b5ed7e0413f3b4cb
Roben Kleene <robenk@gmail.com>
2017-04-18 21:52:10 -0400

    Add some test constants

---
+++ BubbleUpTests/TestConstants.swift
@@ -0,0 +1,11 @@
+//
+//  TestConstants.swift
+//  BubbleUp
+//
+//  Created by Roben Kleene on 4/18/17.
+//  Copyright © 2017 Roben Kleene. All rights reserved.
+//
+
+let defaultTimeout = 20.0
+let testFileContents = "test"
+let testDirectoryName = "test"
